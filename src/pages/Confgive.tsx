0a1e8514255ce468aef20c1a8a695faa0a2ee9f1
BoYueChuang <otis29516368@gmail.com>
2025-04-15 21:57:16 +0800

    拔掉 log

--- src/pages/Confgive.tsx
+++ src/pages/Confgive.tsx
@@ -80,9 +80,6 @@ const CONFGive = () => {
         const appleMerchantId = import.meta.env.VITE_APPLE_MERCHANT_ID || '';
         const googleMerchantId = import.meta.env.VITE_GOOGLE_MERCHANT_ID || '';
 
-        console.log(tappayAppId, tappayAppKey, appleMerchantId, googleMerchantId);
-
-
         if (!tappayAppId || !tappayAppKey) {
             // Error handling
             console.error("Missing TapPay configuration in environment variables.");
